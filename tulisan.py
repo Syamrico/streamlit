144533bc808507e63526b2451d0d6a7762dab19c
Syamrico <140590940+Syamrico@users.noreply.github.com>
2023-09-01 14:08:00 +0700

    Update tulisan.py

--- tulisan.py
+++ tulisan.py
@@ -35,3 +35,18 @@ st.data_editor(
     },
     hide_index=True,
 )
+
+# Cache the dataframe so it's only loaded once
+@st.cache_data
+def load_data():
+    return pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4],
+            "second column": [10, 20, 30, 40],
+        }
+    )
+
+# Boolean to resize the dataframe, stored as a session state variable
+st.checkbox("Use container width", value=False, key="use_container_width")
+
+df = load_data()
