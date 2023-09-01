33cea87ce971c8229eeade8df2f471f4f1706bc8
Syamrico <140590940+Syamrico@users.noreply.github.com>
2023-09-01 12:11:24 +0700

    Update aplikasi.py

--- aplikasi.py
+++ aplikasi.py
@@ -3,21 +3,21 @@ import pandas as pd
 import numpy as np
 # from prediction import predict
 
-st.title('Tebak ini Bunga apa')
-st.markdown('Toy model to play to classify iris flowers into \
+st.title('Tebak ini Bunga apa ??')
+st.markdown('Toy model untuk bermain tebak bunga
             setosa, versicolor, virginica')
 
 st.header("Plant Features")
 col1, col2 = st.columns(2)
 
 with col1:
-  st.text("Sepal characteristics")
-  sepal_l = st.slider('Sepal lenght (cm)', 1.0, 8.0, 0.5)
-  sepal_w = st.slider('Sepal width (cm)', 2.0, 4.4, 0.5)
+  st.text("Karakter bentuk kelopak yang diamati")
+  sepal_l = st.slider('Panjang Kelopak (cm)', 1.0, 8.0, 0.5)
+  sepal_w = st.slider('Lebar Kelopak (cm)', 2.0, 4.4, 0.5)
 
 with col2:
-  st.text("Pepal characteristics")
-  petal_l = st.slider('Petal lenght (cm)', 1.0, 7.0, 0.5)
-  petal_w = st.slider('Petal width (cm)', 0.1, 2.5, 0.5)
+  st.text("Karakter bentuk mahkota yang diamati")
+  petal_l = st.slider('Panjang Mahkota (cm)', 1.0, 7.0, 0.5)
+  petal_w = st.slider('Lebar Mahkota (cm)', 0.1, 2.5, 0.5)
 
-st.button("Predict type of Iris")
+st.button("Prediksi tipe Bunga")
