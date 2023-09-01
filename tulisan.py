6705b0314e50aa6abb82e91846af358f66da3530
Syamrico <140590940+Syamrico@users.noreply.github.com>
2023-09-01 14:09:14 +0700

    Update tulisan.py

--- tulisan.py
+++ tulisan.py
@@ -50,3 +50,4 @@ def load_data():
 st.checkbox("Use container width", value=False, key="use_container_width")
 
 df = load_data()
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
