7bdac8df53d42ed1bf3fad050b1bf1b61aad76a7
Syamrico <140590940+Syamrico@users.noreply.github.com>
2023-09-01 14:19:01 +0700

    Update tulisan.py

--- tulisan.py
+++ tulisan.py
@@ -64,4 +64,4 @@ edited_df = st.data_editor(df)
 
 favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
-st.ballon()
+st.balloons()
